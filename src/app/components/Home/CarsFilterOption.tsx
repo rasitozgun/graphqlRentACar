cbeb4452291d1c97724e65abd2a039338feb9eed
Rasit Ozgun <mrasitozgun@gmail.com>
2023-11-27 11:49:55 +0300

    cars filter option created and building

---
+++ src/app/components/Home/CarsFilterOption.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+function CarsFilterOption() {
+	return (
+		<div className="mt-10 flex items-center">
+			<div>
+				<h2 className="text-[30px] font-bold">Cars Catalog</h2>
+				<h2>Explore cars you might like</h2>
+			</div>
+			<div className="flex gap-5">
+				<select className="select select-bordered w-full max-w-xs">
+					<option disabled selected>
+						Price
+					</option>
+					<option>Min to Max</option>
+					<option>Max to Min</option>
+				</select>
+				<select className="select select-bordered w-full max-w-xs">
+					<option disabled selected>
+						Manufacturer
+					</option>
+					<option>Honda</option>
+					<option>BMW</option>
+					<option>Mercedes</option>
+				</select>
+			</div>
+		</div>
+	);
+}
+
+export default CarsFilterOption;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CarsFilterOption from "./components/Home/CarsFilterOption";
 import Hero from "./components/Home/Hero";
 import SearchInput from "./components/Home/SearchInput";
 
@@ -6,6 +7,7 @@ export default function Home() {
 		<div className="p-5 sm:px-10 md:px-20">
 			<Hero />
 			<SearchInput />
+			<CarsFilterOption />
 		</div>
 	);
 }
